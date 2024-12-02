d94dac22be1820d782ae36aae5d4094d538e61cc
MaidenAsh <ash_siva@outlook.com>
2024-12-02 17:21:16 +0000

    Update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
         
 function createDeck() {
-    let deck = [];
+    var deck = new Array();
 
     for (let i = 0; i<suits.length; i++) {
         for (let j = 0; j<values.length; j++) {
