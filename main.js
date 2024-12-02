fa9a25ceb429b30e88f503ea1e4305ec3363eb21
MaidenAsh <ash_siva@outlook.com>
2024-12-02 16:44:20 +0000

    Rendering

--- main.js
+++ main.js
@@ -5,24 +5,24 @@ var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
         
 function createDeck() {
-    let deck = []
+    let deck = [];
 
     for (let i = 0; i<suits.length; i++) {
         for (let j = 0; j<values.length; j++) {
-            deck.push({Value: values[j], Suit: suits[i]})
+            deck.push({Value: values[j], Suit: suits[i]});
         }
     }
             
     for (let i = 0; i<1000; i++) {
-        let positionA = Math.floor(Math.random()*deck.length)
-        let positionB = Math.floor(Math.random()*deck.length)
-        let mid = deck[positionA]
+        let positionA = Math.floor(Math.random()*deck.length);
+        let positionB = Math.floor(Math.random()*deck.length);
+        let mid = deck[positionA];
                 
-        deck[positionA] = deck[positionB]
-        deck[positionB] = deck[mid] 
-        }
+        deck[positionA] = deck[positionB];
+        deck[positionB] = deck[mid]; 
+    }
 
-    return deck
+    return deck;
 }
 
 function renderDeck(deck) {
@@ -44,6 +44,11 @@ function renderDeck(deck) {
 	}
  }
 
-let test = createDeck();
-console.log(test);
-renderDeck(test);
+ function load()
+ {
+     deck = createDeck();
+     renderDeck(deck);
+ }
+ 
+ window.onload = load;
+                 
