e68a6e4ddc67093775a7ca48be14094098a27229
MaidenAsh <ash_siva@outlook.com>
2024-12-03 18:53:56 +0000

    Added cardsets and drawing

--- test2.js
+++ test2.js
@@ -1,29 +1,70 @@
 
-var suits = ["spades", "diamonds", "clubs", "hearts"];
-var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-        
-function createDeck() {
-    let deck = []
+        var suits = ["spades", "diamonds", "clubs", "hearts"];
+        var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]; 
+        let gameRunning;
+        let downcard1 =[]
+        let downcard2 =[]
+        let upcard1 =[]
+        let upcard2 =[]
+        let handcard1 =[]
+        let handcard2 =[]
+        let stack = []
 
-    for (let i = 0; i<suits.length; i++) {
-        for (let j = 0; j<values.length; j++) {
-            deck.push({Value: values[j], Suit: suits[i]});
+        function initGame() {
+            gameRunning = true;
+            let deck = createDeck();
+            deal(deck);
+            draw(deck);
         }
-    }
 
-    deck.push({Value: "Joker", Suit: "red"})
-    deck.push({Value: "Joker", Suit: "black"})
+        function createDeck() {
+            let deck = []
+
+            for (let i = 0; i<suits.length; i++) {
+                for (let j = 0; j<values.length; j++) {
+                    deck.push({Value: values[j], Suit: suits[i]})
+                }
+            }
+
+            deck.push({Value: "Joker", Suit: "red"})
+            deck.push({Value: "Joker", Suit: "black"})
             
-    for (let x = 0; x<1000; x++) {
-        let positionA = Math.floor(Math.random()*deck.length)
-        let positionB = Math.floor(Math.random()*deck.length)
-        let mid = deck[positionA]
+            for (let i = 0; i<1000; i++) {
+                let positionA = Math.floor(Math.random()*deck.length)
+                let positionB = Math.floor(Math.random()*deck.length)
+                let mid = deck[positionA]
                 
-        deck[positionA] = deck[positionB]
-        deck[positionB] = mid 
-    }
-    return deck
-}
+                deck[positionA] = deck[positionB]
+                deck[positionB] = mid
+            }
+            return deck
+        }
 
-let test = createDeck()
-console.log(test)
+        function deal(deck) {
+            while (downcard1.length<3) {
+                downcard1.push(deck.pop())
+            }
+            while (downcard2.length<3) {
+                downcard2.push(deck.pop())
+            }
+            while (upcard1.length<3) {
+                upcard1.push(deck.pop())
+            }
+            while (upcard2.length<3) {
+                upcard2.push(deck.pop())
+            }
+        }
+
+        function draw(deck) {
+            if (deck.length == 0) {
+                return
+            }
+            while (handcard1.length<3) {
+                handcard1.push(deck.pop())
+            }
+            while (handcard2.length<3) {
+                handcard2.push(deck.pop())
+            }
+        }
+
+        initGame();
