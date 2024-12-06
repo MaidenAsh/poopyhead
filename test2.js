0dc2b55438dc6d24c09e488909fd854c6d29b7fc
MaidenAsh <ash_siva@outlook.com>
2024-12-06 17:55:44 +0000

    Updated rendering

--- test2.js
+++ test2.js
@@ -1,80 +1,135 @@
 
-        var suits = ["spades", "diamonds", "clubs", "hearts"];
-        var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]; 
-        let gameRunning;
-        let downcard1 =[]
-        let downcard2 =[]
-        let upcard1 =[]
-        let upcard2 =[]
-        let handcard1 =[]
-        let handcard2 =[]
-        let stack = []
 
-        function initGame() {
-            gameRunning = true;
-            let deck = createDeck();
-            deal(deck);
-            draw(deck);
-        }
+        //function test() {
+           // ctx.drawImage(document.getElementById("spades2"), 200, 662, 80, 120);
+           // ctx.drawImage(document.getElementById("redBack"), 240, 510, 80, 120);  
+       // }
+        
+       var suits = ["spades", "diamonds", "clubs", "hearts"];
+       var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]; 
+       let gameRunning;
+       let downcard1 = [];
+       let downcard2 = [];
+       let upcard1 = [];
+       let upcard2 = [];
+       let handcard1 = [];
+       let handcard2 = [];
+       let selected = [];
+       let stack = []; 
+       let cw = 80;
+       let ch = 120;
 
-        function createDeck() {
-            let deck = []
+       function initGame() {
+           gameRunning = true;
+           let deck = createDeck();
+           deal(deck);
+           draw(deck);
+           renderCards();
+           console.log(deck);
+           console.log(deck.length);
+       }
 
-            for (let i = 0; i<suits.length; i++) {
-                for (let j = 0; j<values.length; j++) {
-                    deck.push({Value: values[j], Suit: suits[i]})
-                }
-            }
+       function gameLoop() {
+           draw(deck);
+           renderCards();
+       }
 
-            deck.push({Value: "Joker", Suit: "red"})
-            deck.push({Value: "Joker", Suit: "black"})
-            
-            for (let i = 0; i<1000; i++) {
-                let positionA = Math.floor(Math.random()*deck.length)
-                let positionB = Math.floor(Math.random()*deck.length)
-                let mid = deck[positionA]
-                
-                deck[positionA] = deck[positionB]
-                deck[positionB] = mid
-            }
-            return deck
-        }
+       function createDeck() {
+           let deck = []
 
-        function deal(deck) {
-            while (downcard1.length<3) {
-                downcard1.push(deck.pop())
-            }
-            while (downcard2.length<3) {
-                downcard2.push(deck.pop())
-            }
-            while (upcard1.length<3) {
-                upcard1.push(deck.pop())
-            }
-            while (upcard2.length<3) {
-                upcard2.push(deck.pop())
-            }
-        }
+           for (let i = 0; i<suits.length; i++) {
+               for (let j = 0; j<values.length; j++) {
+                   deck.push({Value: values[j], Suit: suits[i]})
+               }
+           }
 
-        function draw(deck) {
-            if (deck.length == 0) {
-                return
-            }
-            while (handcard1.length<3) {
-                handcard1.push(deck.pop())
-            }
-            while (handcard2.length<3) {
-                handcard2.push(deck.pop())
-            }
-        }
+           deck.push({Value: "Joker", Suit: "red"})
+           deck.push({Value: "Joker", Suit: "black"})
+                   
+           for (let i = 0; i<1000; i++) {
+               let positionA = Math.floor(Math.random()*deck.length)
+               let positionB = Math.floor(Math.random()*deck.length)
+               let mid = deck[positionA]
+                       
+               deck[positionA] = deck[positionB]
+               deck[positionB] = mid
+           }
+           return deck
+       }
 
-        initGame();
+       function deal(deck) {
+           while (downcard1.length<3) {
+               downcard1.push(deck.pop())
+           }
+           while (downcard2.length<3) {
+               downcard2.push(deck.pop())
+           }
+           while (upcard1.length<3) {
+               upcard1.push(deck.pop())
+           }   
+           while (upcard2.length<3) {
+               upcard2.push(deck.pop())
+           }
+       }
 
-        function formString(card) {
-            let string = ''
-            return string += card.Suit + card.Value + '.png'
-        }
+       function draw(deck) {
+           if (deck.length == 0) {
+               return
+           }
+           while (handcard1.length<3) {
+               handcard1.push(deck.pop())
+           }
+           while (handcard2.length<3) {
+               handcard2.push(deck.pop())
+           }
+       }
 
-        let deck = createDeck()
+       function formString(card) {
+           let string = ''
+               return string += card.Suit + card.Value
+       }
+               
+       function renderCards() {
+           for (let i = 0; i<downcard1.length; i++) {
+               let x = i*120 + 240
+               //ctx.drawImage(document.getElementById("redBack"), x, 510, 80, 120)
+           }     
+               
+           for (let i = 0; i<downcard2.length; i++) {
+               let x = i*120 + 240
+               //ctx.drawImage(document.getElementById("blueBack"), x, 170, 80, 120)
+           }    
+           
+           for (let i = 0; i<upcard1.length; i++) {
+               let s = formString(upcard1[i])
+               let x = i*120 + 240
+               //ctx.drawImage(document.getElementById(s), x, 510, 80, 120)
+           }    
 
-        console.log(deck[0])
-        console.log(formString(deck[0]))
+           for (let i = 0; i<upcard2.length; i++) {
+               let s = formString(upcard2[i])
+               let x = i*120 + 240
+               //ctx.drawImage(document.getElementById(s), x, 170, 80, 120)
+           } 
+
+           for (let i = 0; i<handcard1.length; i++) {
+               let s = formString(handcard1[i])
+               console.log(handcard1[i])
+               let gap = ((400-(80*handcard1.length))/(handcard1.length + 1))
+               let interval = 80 + gap
+               let initial = 200 + gap
+               let x = i*interval + initial
+               //ctx.drawImage(document.getElementById(s), x, 680, 80, 120)
+           } 
+
+           for (let i = 0; i<handcard2.length; i++) { 
+            console.log(handcard2[i])
+               let gap = ((400-(80*handcard2.length))/(handcard2.length + 1))
+               let interval = 80 + gap
+               let initial = 200 + gap
+               let x = i*interval + initial
+               //ctx.drawImage(document.getElementById(blueBack), x, 0, 80, 120)
+           } 
+
+       }          
+       initGame()
